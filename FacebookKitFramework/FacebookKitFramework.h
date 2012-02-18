23001107df05ce90247ec9440d3810b2a3484759
Dmitry Stadnik <dimzzy@gmail.com>
2012-02-18 15:30:13 +0100

    Initial Commit

---
+++ FacebookKitFramework/FacebookKitFramework.h
@@ -0,0 +1,13 @@
+//
+//  FacebookKitFramework.h
+//  FacebookKitFramework
+//
+//  Created by Stadnik Dmitry on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FacebookKitFramework : NSObject
+
+@end
